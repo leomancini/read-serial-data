d87fb07f9a6c516b62516e18c022ed7dfdb5e220
Leo Mancini <mancini@leo.gd>
2024-05-26 15:34:48 -0400

    Adding buttons and potentiometers to Arduino script

--- arduino/output-data/output-data.ino
+++ arduino/output-data/output-data.ino
@@ -1,30 +1,45 @@
-// Constants for the pin numbers
+// Buttons
 const int buttonPins[] = {3, 5, 7, 15, 14, 16};
 const int numButtons = 6;
-int buttonState[numButtons]; // Array to hold the read values
+int buttonState[numButtons];
+
+// Potentiometers
+const int potPins[] = {A0, A1, A2, A3, A6, A7, A8, A9, A10};
+const int numPots = 9;
+int potValue[numPots];
 
 void setup() {
-Serial.begin(9600); // Start serial communication at 9600 baud.
-
-// Initialize each button pin as an input with the internal pull-up resistor
-for (int i = 0; i < numButtons; i++) {
-pinMode(buttonPins[i], INPUT_PULLUP);
-}
+  Serial.begin(9600);
+  
+  for (int i = 0; i < numButtons; i++) {
+    pinMode(buttonPins[i], INPUT_PULLUP);
+  }
+  
+  for (int i = 0; i < numPots; i++) {
+    pinMode(potPins[i], INPUT);
+  }
 }
 
 void loop() {
-// Read each button state and print it
-for (int i = 0; i < numButtons; i++) {
-// Read the button state
-buttonState[i] = digitalRead(buttonPins[i]);
+  for (int i = 0; i < numButtons; i++) {
+    buttonState[i] = digitalRead(buttonPins[i]);
 
-// Print the state to the Serial Monitor
-Serial.print("Button ");
-Serial.print(i + 1);
-Serial.print(": ");
-Serial.println(buttonState[i] == LOW ? "Pressed" : "Not Pressed");
-}
+    Serial.print("button_");
+    Serial.print(i + 1);
+    Serial.print("=");
+    Serial.print(buttonState[i] == LOW ? "true" : "false");
+    Serial.print(";");
+  }
 
-// Add a delay to reduce the output frequency
-delay(100);
+  for (int i = 0; i < numPots; i++) {
+    potValue[i] = analogRead(potPins[i]);
+    Serial.print("potentiometer_");
+    Serial.print(i + 1);
+    Serial.print("=");
+    Serial.print(potValue[i]);
+    Serial.print(";");
+  }
+  
+  Serial.println("");
+  delay(100);
 }
